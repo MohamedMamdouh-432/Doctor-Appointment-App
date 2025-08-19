9efc75c5b84553c8273e87f314e765a244a12252
MohamedMamdouh-432 <mohammedmamdouh432@gmail.com>
2025-08-20 02:37:53 +0300

    First Commit

---
+++ android/app/src/main/kotlin/com/example/testing_app/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.testing_app
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "native.android/jobs"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result -> 
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else if (call.method == "calcSum") {
+                val v1: Int? = call.argument<Int>("v1")
+                val v2: Int? = call.argument<Int>("v2")
+
+                if (v1 != null && v2 != null) {
+                    val sum = calculateSum(v1, v2)
+                    result.success(sum)
+                } else {
+                    result.error("INVALID_ARGS", "Arguments must not be null", null)
+                }
+            }
+            else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+
+        return batteryLevel
+    }
+    
+    fun calculateSum(a: Int, b: Int): Int {
+        return a + b
+    }
+}
