eaa3f991c0c4daa90abba16976f388a450d99df2
MohamedMamdouh-432 <mohammedmamdouh432@gmail.com>
2025-09-29 22:00:32 +0300

    setup fastlane with deployment on firebase app distribution

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:testing_app/core/di/dependency_injection.dart';
+import 'package:testing_app/firebase_options.dart';
 import 'package:testing_app/launcher.dart';
 
 void main() async {
@@ -12,7 +14,8 @@ void main() async {
       statusBarBrightness: Brightness.light,
     ),
   );
-  setupDI();
+  await setupDI();
   await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Launcher());
 }
